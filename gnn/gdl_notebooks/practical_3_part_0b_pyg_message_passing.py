4a1f3d74fae5d432adbcea2a6ace8c8bddadde3f
shilem2 <moshe.shilemay@medtronic.com>
2024-02-06 18:42:04 +0200

    wip practical 3 part 0b complete invariance and equivariance unit testing

---
+++ gnn/gdl_notebooks/practical_3_part_0b_pyg_message_passing.py
@@ -0,0 +1,366 @@
+"""
+Course page:
+https://geometricdeeplearning.com/lectures/
+
+Original colab notebook:
+https://colab.research.google.com/drive/1p9vlVAUcQZXQjulA7z_FyPrB9UXFATrR
+
+My copy:
+https://colab.research.google.com/drive/1UW-rfX-IKa4TCXF-vhjSNJmc-E-nZ-pw#scrollTo=2xcV8Yb148Kq
+"""
+
+import os
+import time
+import random
+import numpy as np
+
+from scipy.stats import ortho_group
+
+import torch
+import torch.nn.functional as F
+from torch.nn import Linear, ReLU, BatchNorm1d, Module, Sequential
+
+import torch_geometric
+from torch_geometric.data import Data
+from torch_geometric.data import Batch
+from torch_geometric.datasets import QM9
+import torch_geometric.transforms as T
+from torch_geometric.utils import remove_self_loops, to_dense_adj, dense_to_sparse
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import MessagePassing, global_mean_pool
+from torch_geometric.datasets import QM9
+from torch_scatter import scatter
+
+import rdkit.Chem as Chem
+from rdkit.Geometry.rdGeometry import Point3D
+from rdkit.Chem import QED, Crippen, rdMolDescriptors, rdmolops, Draw
+# from rdkit.Chem.Draw import IPythonConsole
+
+import py3Dmol
+from rdkit.Chem import AllChem
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+# from google.colab import files
+# from IPython.display import HTML
+
+print("PyTorch version {}".format(torch.__version__))
+print("PyG version {}".format(torch_geometric.__version__))
+
+
+from gnn.gdl_notebooks.utils import seed
+from gnn.gdl_notebooks.practical_3_part_0a_molecule_pred_pyg import CompleteGraph, SetTarget
+
+
+
+class MPNNLayer(MessagePassing):
+    def __init__(self, emb_dim=64, edge_dim=4, aggr='add'):
+        """Message Passing Neural Network Layer
+
+        Args:
+            emb_dim: (int) - hidden dimension `d`
+            edge_dim: (int) - edge feature dimension `d_e`
+            aggr: (str) - aggregation function `\oplus` (sum/mean/max)
+        """
+        # Set the aggregation function
+        super().__init__(aggr=aggr)
+
+        self.emb_dim = emb_dim
+        self.edge_dim = edge_dim
+
+        # MLP `\psi` for computing messages `m_ij`
+        # Implemented as a stack of Linear->BN->ReLU->Linear->BN->ReLU
+        # dims: (2d + d_e) -> d
+        self.mlp_msg = Sequential(
+            Linear(2*emb_dim + edge_dim, emb_dim), BatchNorm1d(emb_dim), ReLU(),
+            Linear(emb_dim, emb_dim), BatchNorm1d(emb_dim), ReLU()
+          )
+
+        # MLP `\phi` for computing updated node features `h_i^{l+1}`
+        # Implemented as a stack of Linear->BN->ReLU->Linear->BN->ReLU
+        # dims: 2d -> d
+        self.mlp_upd = Sequential(
+            Linear(2*emb_dim, emb_dim), BatchNorm1d(emb_dim), ReLU(),
+            Linear(emb_dim, emb_dim), BatchNorm1d(emb_dim), ReLU()
+          )
+
+    def forward(self, h, edge_index, edge_attr):
+        """
+        The forward pass updates node features `h` via one round of message passing.
+
+        As our MPNNLayer class inherits from the PyG MessagePassing parent class,
+        we simply need to call the `propagate()` function which starts the
+        message passing procedure: `message()` -> `aggregate()` -> `update()`.
+
+        The MessagePassing class handles most of the logic for the implementation.
+        To build custom GNNs, we only need to define our own `message()`,
+        `aggregate()`, and `update()` functions (defined subsequently).
+
+        Args:
+            h: (n, d) - initial node features
+            edge_index: (e, 2) - pairs of edges (i, j)
+            edge_attr: (e, d_e) - edge features
+
+        Returns:
+            out: (n, d) - updated node features
+        """
+        out = self.propagate(edge_index, h=h, edge_attr=edge_attr)
+        return out
+
+    def message(self, h_i, h_j, edge_attr):
+        """Step (1) Message
+
+        The `message()` function constructs messages from source nodes j
+        to destination nodes i for each edge (i, j) in `edge_index`.
+
+        The arguments can be a bit tricky to understand: `message()` can take
+        any arguments that were initially passed to `propagate`. Additionally,
+        we can differentiate destination nodes and source nodes by appending
+        `_i` or `_j` to the variable name, e.g. for the node features `h`, we
+        can use `h_i` and `h_j`.
+
+        This part is critical to understand as the `message()` function
+        constructs messages for each edge in the graph. The indexing of the
+        original node features `h` (or other node variables) is handled under
+        the hood by PyG.
+
+        Args:
+            h_i: (e, d) - destination node features
+            h_j: (e, d) - source node features
+            edge_attr: (e, d_e) - edge features
+
+        Returns:
+            msg: (e, d) - messages `m_ij` passed through MLP `\psi`
+        """
+        msg = torch.cat([h_i, h_j, edge_attr], dim=-1)
+        return self.mlp_msg(msg)
+
+    def aggregate(self, inputs, index):
+        """Step (2) Aggregate
+
+        The `aggregate` function aggregates the messages from neighboring nodes,
+        according to the chosen aggregation function ('sum' by default).
+
+        Args:
+            inputs: (e, d) - messages `m_ij` from destination to source nodes
+            index: (e, 1) - list of source nodes for each edge/message in `input`
+
+        Returns:
+            aggr_out: (n, d) - aggregated messages `m_i`
+        """
+        return scatter(inputs, index, dim=self.node_dim, reduce=self.aggr)
+
+    def update(self, aggr_out, h):
+        """
+        Step (3) Update
+
+        The `update()` function computes the final node features by combining the
+        aggregated messages with the initial node features.
+
+        `update()` takes the first argument `aggr_out`, the result of `aggregate()`,
+        as well as any optional arguments that were initially passed to
+        `propagate()`. E.g. in this case, we additionally pass `h`.
+
+        Args:
+            aggr_out: (n, d) - aggregated messages `m_i`
+            h: (n, d) - initial node features
+
+        Returns:
+            upd_out: (n, d) - updated node features passed through MLP `\phi`
+        """
+        upd_out = torch.cat([h, aggr_out], dim=-1)
+        return self.mlp_upd(upd_out)
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}(emb_dim={self.emb_dim}, aggr={self.aggr})')
+
+
+class MPNNModel(Module):
+    def __init__(self, num_layers=4, emb_dim=64, in_dim=11, edge_dim=4, out_dim=1):
+        """Message Passing Neural Network model for graph property prediction
+
+        Args:
+            num_layers: (int) - number of message passing layers `L`
+            emb_dim: (int) - hidden dimension `d`
+            in_dim: (int) - initial node feature dimension `d_n`
+            edge_dim: (int) - edge feature dimension `d_e`
+            out_dim: (int) - output dimension (fixed to 1)
+        """
+        super().__init__()
+
+        # Linear projection for initial node features
+        # dim: d_n -> d
+        self.lin_in = Linear(in_dim, emb_dim)
+
+        # Stack of MPNN layers
+        self.convs = torch.nn.ModuleList()
+        for layer in range(num_layers):
+            self.convs.append(MPNNLayer(emb_dim, edge_dim, aggr='add'))
+
+        # Global pooling/readout function `R` (mean pooling)
+        # PyG handles the underlying logic via `global_mean_pool()`
+        self.pool = global_mean_pool
+
+        # Linear prediction head
+        # dim: d -> out_dim
+        self.lin_pred = Linear(emb_dim, out_dim)
+
+    def forward(self, data):
+        """
+        Args:
+            data: (PyG.Data) - batch of PyG graphs
+
+        Returns:
+            out: (batch_size, out_dim) - prediction for each graph
+        """
+        h = self.lin_in(data.x) # (n, d_n) -> (n, d)
+
+        for conv in self.convs:
+            h = h + conv(h, data.edge_index, data.edge_attr) # (n, d) -> (n, d)
+            # Note that we add a residual connection after each MPNN layer
+
+        h_graph = self.pool(h, data.batch) # (n, d) -> (batch_size, d)
+
+        out = self.lin_pred(h_graph) # (batch_size, d) -> (batch_size, 1)
+
+        return out.view(-1)
+
+def permute_graph(data, perm):
+    """Helper function for permuting PyG Data object attributes consistently.
+    """
+    # Permute the node attribute ordering
+    data.x = data.x[perm]
+    data.pos = data.pos[perm]
+    data.z = data.z[perm]
+    data.batch = data.batch[perm]
+
+    # Permute the edge index
+    adj = to_dense_adj(data.edge_index)
+    adj = adj[:, perm, :]
+    adj = adj[:, :, perm]
+    data.edge_index = dense_to_sparse(adj)[0]
+
+    # Note:
+    # (1) While we originally defined the permutation matrix P as only having
+    #     entries 0 and 1, its implementation via `perm` uses indexing into
+    #     torch tensors, instead.
+    # (2) It is cumbersome to permute the edge_attr, so we set it to constant
+    #     dummy values. For any experiments beyond unit testing, all GNN models
+    #     use the original edge_attr.
+
+    return data
+
+def permutation_invariance_unit_test(module, dataloader):
+    """Unit test for checking whether a module (GNN model) is
+    permutation invariant.
+    """
+    it = iter(dataloader)
+    data = next(it)
+
+    # Set edge_attr to dummy values (for simplicity)
+    data.edge_attr = torch.zeros(data.edge_attr.shape)
+
+    # Forward pass on original example
+    out_1 = module(data)
+
+    # Create random permutation
+    perm = torch.randperm(data.x.shape[0])
+    data = permute_graph(data, perm)
+
+    # Forward pass on permuted example
+    out_2 = module(data)
+
+    # Check whether output varies after applying transformations
+    return torch.allclose(out_1, out_2, atol=1e-04)
+
+
+def permutation_equivariance_unit_test(module, dataloader):
+    """Unit test for checking whether a module (GNN layer) is
+    permutation equivariant.
+    """
+    it = iter(dataloader)
+    data = next(it)
+
+    # Set edge_attr to dummy values (for simplicity)
+    data.edge_attr = torch.zeros(data.edge_attr.shape)
+
+    # Forward pass on original example
+    out_1 = module(data.x, data.edge_index, data.edge_attr)
+
+    # Create random permutation
+    perm = torch.randperm(data.x.shape[0])
+    data = permute_graph(data, perm)
+
+    # Forward pass on permuted example
+    out_2 = module(data.x, data.edge_index, data.edge_attr)
+
+    # Check whether output varies after applying transformations
+    return torch.allclose(out_1[perm], out_2, atol=1e-04)
+
+
+def main():
+
+    # Coding the basic Message Passing Neural Network Layer
+
+    # we will inherit from the `MessagePassing` base class, which automatically takes care of message propagation and is extremely useful to develop advanced GNN models.
+    # To implement a custom MPNN, the user only needs to define the behaviour of the `message` (i.e. $\psi$), the `aggregate`(i.e. $\oplus$), and `update` (i.e. $\phi$) functions.
+    # You may also refer to the [PyG documentation](https://pytorch-geometric.readthedocs.io/en/latest/notes/create_gnn.html) for implementing custom message passing layers.
+
+    # get data
+    # set different generators random seed
+    seed(0)
+
+    # load QM9 dataset
+    path = './qm9'
+    target = 0
+
+    # Transforms which are applied during data loading:
+    # (1) Fully connect the graphs, (2) Select the target/label
+    transform = T.Compose([CompleteGraph(), SetTarget()])
+
+    # Load the QM9 dataset with the transforms defined
+    dataset = QM9(path, transform=transform)
+
+    # Normalize targets per data sample to mean = 0 and std = 1.
+    mean = dataset.data.y.mean(dim=0, keepdim=True)
+    std = dataset.data.y.std(dim=0, keepdim=True)
+    dataset.data.y = (dataset.data.y - mean) / std
+    mean, std = mean[:, target].item(), std[:, target].item()
+
+    # The QM9 dataset has over 130,000 molecular graphs!
+    # Let us create a more tractable sub-set of 3,000 molecular graphs for the purposes of this practical and separate it into training, validation, and test sets.
+
+    print(f"Total number of samples: {len(dataset)}.")
+
+    # Split datasets (in case of using the full dataset)
+    # test_dataset = dataset[:10000]
+    # val_dataset = dataset[10000:20000]
+    # train_dataset = dataset[20000:]
+
+    # Split datasets (our 3K subset)
+    train_dataset = dataset[:1000]
+    val_dataset = dataset[1000:2000]
+    test_dataset = dataset[2000:3000]
+    print(f"Created dataset splits with {len(train_dataset)} training, {len(val_dataset)} validation, {len(test_dataset)} test samples.")
+
+    # Instantiate temporary model, layer, and dataloader for unit testing
+    layer = MPNNLayer(emb_dim=11, edge_dim=4)
+    model = MPNNModel(num_layers=4, emb_dim=64, in_dim=11, edge_dim=4, out_dim=1)
+    dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+
+    # Permutation invariance unit test for MPNN model
+    print(f"Is {type(model).__name__} permutation invariant? --> {permutation_invariance_unit_test(model, dataloader)}!")
+
+    # Permutation equivariance unit for MPNN layer
+    print(f"Is {type(layer).__name__} permutation equivariant? --> {permutation_equivariance_unit_test(layer, dataloader)}!")
+
+    pass
+
+
+if __name__ == '__main__':
+
+    main()
+
+    pass
