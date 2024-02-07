59afee814a876171dc2c65b1dc7e1a20702c4592
shilem2 <moshe.shilemay@medtronic.com>
2024-02-07 14:50:22 +0200

    wip practical_3_part_3 implement MPNN layer and model invariant to rotation and translation! unit test are passing

--- gnn/gdl_notebooks/practical_3_part_3_mpnn_3d_rot_trans_invariant.py
+++ gnn/gdl_notebooks/practical_3_part_3_mpnn_3d_rot_trans_invariant.py
@@ -73,7 +73,7 @@ def rot_trans_invariance_unit_test(module, dataloader):
     #       The functionality for layers will be useful subsequently.
     if isinstance(module, MPNNModel):
         out_1 = module(data)
-    else: # if ininstance(module, MessagePassing):
+    else: # if isinstance(module, MessagePassing):
         out_1 = module(data.x, data.pos, data.edge_index, data.edge_attr)
 
     Q = random_orthogonal_matrix(dim=3)
@@ -82,6 +82,8 @@ def rot_trans_invariance_unit_test(module, dataloader):
     # Perform random rotation + translation on data.
     #
     # data.pos = ...
+
+    data.pos = data.pos @ Q + t
     # ==========================================
 
     # Forward pass on rotated + translated example
@@ -96,9 +98,13 @@ def rot_trans_invariance_unit_test(module, dataloader):
     # return torch.allclose(..., atol=1e-04)
     # ==========================================
 
+    is_invariant = torch.allclose(out_1, out_2, atol=1e-04)
+
+    return is_invariant
+
 
 class CoordMPNNModel(MPNNModel):
-    def __init__(self, num_layers=4, emb_dim=64, in_dim=11, edge_dim=4, out_dim=1, coord_dim=3):
+    def __init__(self, num_layers=4, emb_dim=64, in_dim=11, edge_dim=4, out_dim=1):
         """Message Passing Neural Network model for graph property prediction
 
         This model uses both node features and coordinates as inputs.
@@ -121,7 +127,7 @@ class CoordMPNNModel(MPNNModel):
         # dim: ??? -> d
         # self.lin_in = ...
 
-        self.lin_in = Linear(in_dim + coord_dim, emb_dim)
+        self.lin_in = Linear(in_dim, emb_dim)
 
         # ==========================================
 
@@ -151,8 +157,7 @@ class CoordMPNNModel(MPNNModel):
         #
         # h = ...
 
-        node_feat = torch.cat((data.x, data.pos), dim=1)
-        h = self.lin_in(node_feat)
+        h = self.lin_in(data.x)
 
         # ==========================================
 
@@ -167,16 +172,226 @@ class CoordMPNNModel(MPNNModel):
         return out.view(-1)
 
 
+class InvariantMPNNLayer(MessagePassing):
+    def __init__(self, emb_dim=64, edge_dim=4, aggr='add', coord_dim=1):
+        """Message Passing Neural Network Layer
+
+        This layer is invariant to 3D rotations and translations.
+
+        Args:
+            emb_dim: (int) - hidden dimension `d`
+            edge_dim: (int) - edge feature dimension `d_e`
+            aggr: (str) - aggregation function `\oplus` (sum/mean/max)
+        """
+        # Set the aggregation function
+        super().__init__(aggr=aggr)
+
+        self.emb_dim = emb_dim
+        self.edge_dim = edge_dim
+
+        # ============ YOUR CODE HERE ==============
+        # MLP `\psi` for computing messages `m_ij`
+        # dims: (???) -> d
+        #
+        # self.mlp_msg = Sequential(...)
+
+        self.mlp_msg = Sequential(
+            Linear(2*emb_dim + edge_dim + coord_dim, emb_dim), BatchNorm1d(emb_dim), ReLU(),
+            Linear(emb_dim, emb_dim), BatchNorm1d(emb_dim), ReLU()
+          )
+        # ==========================================
+
+        # MLP `\phi` for computing updated node features `h_i^{l+1}`
+        # dims: 2d -> d
+        self.mlp_upd = Sequential(
+            Linear(2*emb_dim, emb_dim), BatchNorm1d(emb_dim), ReLU(),
+            Linear(emb_dim, emb_dim), BatchNorm1d(emb_dim), ReLU()
+          )
+
+    def forward(self, h, pos, edge_index, edge_attr):
+        """
+        The forward pass updates node features `h` via one round of message passing.
+
+        Args:
+            h: (n, d) - initial node features
+            pos: (n, 3) - initial node coordinates
+            edge_index: (e, 2) - pairs of edges (i, j)
+            edge_attr: (e, d_e) - edge features
+
+        Returns:
+            out: (n, d) - updated node features
+        """
+        # ============ YOUR CODE HERE ==============
+        # Notice that the `forward()` function has a new argument
+        # `pos` denoting the initial node coordinates. Your task is
+        # to update the `propagate()` function in order to pass `pos`
+        # to the `message()` function along with the other arguments.
+        #
+        # out = self.propagate(...)
+        # return out
+        # ==========================================
+
+        out = self.propagate(edge_index, h=h, pos=pos, edge_attr=edge_attr)
+        return out
+
+    # ============ YOUR CODE HERE ==============
+    # Write a custom `message()` function that takes as arguments the
+    # source and destination node features, node coordiantes, and `edge_attr`.
+    # Incorporate the coordinates `pos` into the message computation such
+    # that the messages are invariant to rotations and translations.
+    # This will ensure that the overall layer is also invariant.
+    #
+    # def message(self, ...):
+    # """The `message()` function constructs messages from source nodes j
+    #    to destination nodes i for each edge (i, j) in `edge_index`.
+    #
+    #    Args:
+    #        ...
+    #
+    #    Returns:
+    #        ...
+    # """
+    #   ...
+    #   msg = ...
+    #   return self.mlp_msg(msg)
+    # ==========================================
+
+    def message(self, h_i, h_j, pos_i, pos_j, edge_attr):
+        """The `message()` function constructs messages from source nodes j
+           to destination nodes i for each edge (i, j) in `edge_index`.
+
+           Args:
+               ...
+
+           Returns:
+               ...
+        """
+
+        dist = torch.linalg.norm(pos_i - pos_j, dim=1).unsqueeze(1)
+        msg = torch.cat([h_i, h_j, edge_attr, dist], dim=-1)
+
+        return self.mlp_msg(msg)
+
+    def aggregate(self, inputs, index):
+        """The `aggregate` function aggregates the messages from neighboring nodes,
+        according to the chosen aggregation function ('sum' by default).
+
+        Args:
+            inputs: (e, d) - messages `m_ij` from destination to source nodes
+            index: (e, 1) - list of source nodes for each edge/message in `input`
+
+        Returns:
+            aggr_out: (n, d) - aggregated messages `m_i`
+        """
+        return scatter(inputs, index, dim=self.node_dim, reduce=self.aggr)
+
+    def update(self, aggr_out, h):
+        """The `update()` function computes the final node features by combining the
+        aggregated messages with the initial node features.
+
+        Args:
+            aggr_out: (n, d) - aggregated messages `m_i`
+            h: (n, d) - initial node features
+
+        Returns:
+            upd_out: (n, d) - updated node features passed through MLP `\phi`
+        """
+        upd_out = torch.cat([h, aggr_out], dim=-1)
+        return self.mlp_upd(upd_out)
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}(emb_dim={self.emb_dim}, aggr={self.aggr})')
+
+
+class InvariantMPNNModel(MPNNModel):
+    def __init__(self, num_layers=4, emb_dim=64, in_dim=11, edge_dim=4, out_dim=1, coord_dim=3):
+        """Message Passing Neural Network model for graph property prediction
+
+        This model uses both node features and coordinates as inputs, and
+        is invariant to 3D rotations and translations.
+
+        Args:
+            num_layers: (int) - number of message passing layers `L`
+            emb_dim: (int) - hidden dimension `d`
+            in_dim: (int) - initial node feature dimension `d_n`
+            edge_dim: (int) - edge feature dimension `d_e`
+            out_dim: (int) - output dimension (fixed to 1)
+            coord_dim: (int) - coordination dimension (3 - for 3D coordinates)
+        """
+        super().__init__()
+
+        # Linear projection for initial node features
+        # dim: d_n -> d
+        self.lin_in = Linear(in_dim, emb_dim)
+
+        # Stack of invariant MPNN layers
+        self.convs = torch.nn.ModuleList()
+        for layer in range(num_layers):
+            self.convs.append(InvariantMPNNLayer(emb_dim, edge_dim, aggr='add'))
+
+        # Global pooling/readout function `R` (mean pooling)
+        # PyG handles the underlying logic via `global_mean_pool()`
+        self.pool = global_mean_pool
+
+        # Linear prediction head
+        # dim: d -> out_dim
+        self.lin_pred = Linear(emb_dim, out_dim)
+
+    def forward(self, data):
+        """
+        Args:
+            data: (PyG.Data) - batch of PyG graphs
+
+        Returns:
+            out: (batch_size, out_dim) - prediction for each graph
+        """
+        h = self.lin_in(data.x) # (n, d_n) -> (n, d)
+
+        for conv in self.convs:
+            h = h + conv(h, data.pos, data.edge_index, data.edge_attr) # (n, d) -> (n, d)
+            # Note that we add a residual connection after each MPNN layer
+
+        h_graph = self.pool(h, data.batch) # (n, d) -> (batch_size, d)
+
+        out = self.lin_pred(h_graph) # (batch_size, d) -> (batch_size, 1)
+
+        return out.view(-1)
+
+
 def main():
 
     train_dataset, val_dataset, test_dataset, std = get_qm9_data()
 
+    # Unit test CoordMPNNModel
     # Instantiate temporary model, layer, and dataloader for unit testing
     model = CoordMPNNModel(num_layers=4, emb_dim=64, in_dim=11, edge_dim=4, out_dim=1)
     dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True)
 
     # Rotation and translation invariance unit test for MPNN model
     print(f"Is {type(model).__name__} rotation and translation invariant? --> {rot_trans_invariance_unit_test(model, dataloader)}!")
+
+
+
+    # Unit test CoordMPNNModel
+    # ============ YOUR CODE HERE ==============
+    # Instantiate temporary model, layer, and dataloader for unit testing.
+    # Remember that we are now unit testing the InvariantMPNNModel,
+    # which is  composed of the InvariantMPNNLayer.
+    #
+    # layer = ...
+    # model = ...
+
+    model = InvariantMPNNModel(num_layers=4, emb_dim=64, in_dim=11, edge_dim=4, out_dim=1)
+    layer = InvariantMPNNLayer(emb_dim=11, edge_dim=4, aggr='add', coord_dim=1)
+
+    # ==========================================
+    dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+
+    # Rotation and translation invariance unit test for MPNN model
+    print(f"Is {type(model).__name__} rotation and translation invariant? --> {rot_trans_invariance_unit_test(model, dataloader)}!")
+
+    # Rotation and translation invariance unit test for MPNN layer
+    print(f"Is {type(layer).__name__} rotation and translation invariant? --> {rot_trans_invariance_unit_test(layer, dataloader)}!")
 
     pass
 
